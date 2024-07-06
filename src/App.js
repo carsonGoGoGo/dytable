39ad6b3edfb8c7641aca6b69e13b87634b3e63ce
carson <782610180@qq.com>
2024-07-06 10:18:45 +0800

    第一次提交

--- src/App.js
+++ src/App.js
@@ -5,21 +5,61 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <DynamicTable />
       </header>
     </div>
   );
+}
+
+
+const DynamicTable = () =>{
+    return(
+        <div>
+            <table border="1">
+                <tr>
+                    <th></th>
+                    <th>星期一</th>
+                    <th>星期二</th>
+                    <th>星期三</th>
+                    <th>星期四</th>
+                    <th>星期五</th>
+                </tr>
+                <tr>
+                    <td>上午第一节课</td>
+                    <td>语文课</td>
+                    <td>数学课</td>
+                    <td>生物课</td>
+                    <td>法语课</td>
+                    <td>西班牙课程</td>
+                </tr>
+                <tr>
+                    <td>上午第二节课</td>
+                    <td>语文课</td>
+                    <td>数学课</td>
+                    <td>生物课</td>
+                    <td>法语课</td>
+                    <td>西班牙课程</td>
+                </tr>
+
+                <tr>
+                    <td>上午第三节课</td>
+                    <td>语文课</td>
+                    <td>数学课</td>
+                    <td>生物课</td>
+                    <td>法语课</td>
+                    <td>西班牙课程</td>
+                </tr>
+                <tr>
+                    <td>第一节课</td>
+                    <td>语文课</td>
+                    <td>数学课</td>
+                    <td>生物课</td>
+                    <td>法语课</td>
+                    <td>西班牙课程</td>
+                </tr>
+            </table>
+        </div>
+    )
 }
 
 export default App;
