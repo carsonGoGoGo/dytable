b8c0ec1a6bb4cee252c96bfaee27dc430379503c
carson <782610180@qq.com>
2024-07-06 10:55:46 +0800

    简单的实现了增加一行的效果，需要改善

--- src/App.js
+++ src/App.js
@@ -1,66 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
     return (<div className="App">
-            <header className="App-header">
-                <DynamicTable/>
-            </header>
-        </div>);
+        <header className="App-header">
+            <DynamicTable/>
+        </header>
+    </div>);
 }
 
 
 const DynamicTable = () => {
+    const initData = [{
+        "id": 1,
+        "name": "John Doe",
+        "email": "john@example.com",
+        "phone": "555-555-5555",
+        "address": "123 Main St",
+        "age": 30,
+    }, {
+        "id": 2,
+        "name": "Jane Doe",
+        "email": "jane@example.com",
+        "phone": "555-555-5556",
+        "address": "456 Main St",
+        "age": 33,
+    }, {
+        "id": 3,
+        "name": "Jim Smith",
+        "email": "jane@example.com",
+        "phone": "888-777-5556",
+        "address": "789 Main St",
+        "age": 34,
+    }];
+
+    const [data, setData] = useState(initData);
+
+
+    function handleDeleteRow() {
+
+    }
+
+
+    function handleAddRow() {
+        const newUser  = {
+            id: data.length +1,
+            name: '',
+            email: '',
+            phone: '',
+            address: '',
+            age: '',
+        }
+        setData([...data, newUser]);
+    }
+
     return (<div>
-            <table border="1">
-                <thead>
-                <tr>
-                    <th>星期一</th>
-                    <th>星期二</th>
-                    <th>星期三</th>
-                    <th>星期四</th>
-                    <th>星期五</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>上午第一节课</td>
-                    <td>语文课</td>
-                    <td>数学课</td>
-                    <td>生物课</td>
-                    <td>法语课</td>
-                    <td>西班牙课程</td>
-                </tr>
-                <tr>
-                    <td>上午第二节课</td>
-                    <td>语文课</td>
-                    <td>数学课</td>
-                    <td>生物课</td>
-                    <td>法语课</td>
-                    <td>西班牙课程</td>
-                </tr>
+        <button onClick={handleAddRow}>点击新增一行</button>
+        <table border="1">
+            <thead>
+            <tr>
+                <th></th>
+                <th>名字</th>
+                <th>电子邮件</th>
+                <th>电话号码</th>
+                <th>住址</th>
+                <th>年纪</th>
+            </tr>
+            </thead>
+            <tbody>
+            {data.map((item, index) => (<tr key={item.id}>
+                <td></td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                <td>{item.address}</td>
+                <td>{item.age}</td>
+            </tr>))}
 
-                <tr>
-                    <td>上午第三节课</td>
-                    <td>语文课</td>
-                    <td>数学课</td>
-                    <td>生物课</td>
-                    <td>法语课</td>
-                    <td>西班牙课程</td>
-                </tr>
+            </tbody>
 
-                <tr>
-                    <td>第一节课</td>
-                    <td>语文课</td>
-                    <td>数学课</td>
-                    <td>生物课</td>
-                    <td>法语课</td>
-                    <td>西班牙课程</td>
-                </tr>
-                </tbody>
-
-            </table>
-        </div>)
+        </table>
+        <button onClick={() => {
+            handleDeleteRow()
+        }}>点击删除一行
+        </button>
+    </div>)
 }
 
 export default App;
